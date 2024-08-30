37fcf3c6024894f6d05ee5681c4f997d1b422211
Aakash Rajaraman <156072559+aakashrajaraman2@users.noreply.github.com>
2024-08-30 15:53:48 +0530

    Typo: 'sts' should be 'str'

--- graphrag/model/entity.py
+++ graphrag/model/entity.py
@@ -25,7 +25,7 @@ class Entity(Named):
     name_embedding: list[float] | None = None
     """The semantic (i.e. text) embedding of the entity (optional)."""
 
-    graph_embedding: list[float] | None = None
+    graph_embedding: list[str] | None = None
     """The graph embedding of the entity, likely from node2vec (optional)."""
 
     community_ids: list[str] | None = None
@@ -36,11 +36,12 @@ class Entity(Named):
 
     document_ids: list[str] | None = None
     """List of document IDs in which the entity appears (optional)."""
+    
 
     rank: int | None = 1
     """Rank of the entity, used for sorting (optional). Higher rank indicates more important entity. This can be based on centrality or other metrics."""
 
-    attributes: dict[str, Any] | None = None
+    attributes: dict(str, Any) | None = None
     """Additional attributes associated with the entity (optional), e.g. start time, end time, etc. To be included in the search prompt."""
 
     @classmethod
